e302c7bb052aaeeef5b6c024aa59dda212e45251
titanxrio <Titan150208@gmail.com>
2025-07-03 00:55:20 +0200

    Update vouches.js

--- netlify/functions/vouches.js
+++ netlify/functions/vouches.js
@@ -1,27 +1,27 @@
-const fetch = require(node-fetch);
+const fetch = require("node-fetch");
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
   try {
-    const response = await fetch(http2.58.113.2116969apivouches, {
-      headers {
-        Authorization Bearer e6N7yUMHs2q2U5m7JrQBfMVQjrKwpcQY
+    const response = await fetch("http://2.58.113.211:6969/api/vouches", {
+      headers: {
+        Authorization: "Bearer e6N7yUMHs2q2U5m7JrQBfMVQjrKwpcQY"
       }
     });
 
     const data = await response.json();
 
     return {
-      statusCode 200,
-      body JSON.stringify(data),
-      headers {
-        Access-Control-Allow-Origin ,
-        Content-Type applicationjson
+      statusCode: 200,
+      body: JSON.stringify(data),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
       }
     };
   } catch (err) {
     return {
-      statusCode 500,
-      body JSON.stringify({ error err.toString() })
+      statusCode: 500,
+      body: JSON.stringify({ error: err.toString() })
     };
   }
 };
